6caebdffe0d01dd768fb155dec32ace132cb43c8
jakecronin <jake.cronin4@gmail.com>
2017-09-09 23:09:23 -0400

    added pods

---
+++ VuforiaSampleSwift/RealmManager.swift
@@ -0,0 +1,10 @@
+//
+//  RealmManager.swift
+//  VuforiaSampleSwift
+//
+//  Created by Jake Cronin on 9/9/17.
+//  Copyright © 2017 Yoshihiro Kato. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
