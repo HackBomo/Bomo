71765f025dd30374ef85ff5d7deeb2604d5a0f71
tangert <tyler@angert.com>
2017-10-18 17:22:37 -0400

    cleaning up view controllers

---
+++ VuforiaSampleSwift/SubjectProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SubjectProfileViewController.swift
+//  VuforiaSampleSwift
+//
+//  Created by Tyler Angert on 10/18/17.
+//  Copyright © 2017 Yoshihiro Kato. All rights reserved.
+//
+
+import Foundation
